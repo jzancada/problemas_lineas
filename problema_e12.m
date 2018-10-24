e722bc81b8f9d1ae19632ba5c6cf7cb5419c9fff
jzancada <j.zancada@gmail.com>
2018-10-25 00:38:30 +0200

    corrige algunos errores

--- problema_e12.m
+++ problema_e12.m
@@ -5,10 +5,16 @@ eta = 0.95
 U_R = 132e3 % V
 rp = 1
 cos_phi = 0.9
+% 
+% I = P_R / ( sqrt(3) * U_R * cos_phi)
+% P_joule = 5/100*P_R
+% 
+% R = P_joule / (3*I^2)
+% 
+% longitud_km =  R
 
-I = P_R / ( sqrt(3) * U_R * cos_phi)
-P_joule = 5/100*P_R
 
+P_joule = 5/100*P_R/(1-5/100)
+P_S = P_R + P_joule
 R = P_joule / (3*I^2)
-
 longitud_km =  R
--- problema_e23.m
+++ problema_e23.m
@@ -5,21 +5,21 @@ U_V = 20E3
 gamma_s = 1
 gamma_p = 1
 
-rho = 28.6
+rho = 28.6 % ohm mm^2 / km
 alpha = 4.03E-3
 
 theta_max = 90
-S = 240
+S = 240 % mm^2 
 d_c = 17.9
 D_aisl = 30
 D_ext = 37.1
 tan_delta = 0.004
 epsilon = 2.5
 
-R0 = rho / S
-Rp = R0 * (1+alpha*(theta_max-20))
-R = Rp * (1 + gamma_s + gamma_p)
+R0 = rho / S % ohm / km
+Rp = R0 * (1+alpha*(theta_max-20)) % ohm / km
+R = Rp * (1 + gamma_s + gamma_p) % ohm / km
 
-C = epsilon*1e-9 / (18 * log(D_ext/d_c))
+C = epsilon*1e-9 / (18 * log(D_aisl/d_c)) % F / m
 omega_d = 2*pi*50 * C * (U_V / sqrt(3))^2 * tan_delta
 omega_d_total = 3*omega_d
